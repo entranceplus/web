a697f84dcdfa5510fa88df0137cf49634c2ecc66
sumit-narang <sumitnarang.web@gmail.com>
2018-01-08 19:10:27 +0530

    refactor: changed const to var for variables

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,7 +1,7 @@
-const header = document.getElementById('header');
-const hamburger = header.querySelector('.hamburger');
-const links = header.querySelector('.nav-links');
-const close = header.querySelector('.close-btn');
+var header = document.getElementById('header');
+var hamburger = header.querySelector('.hamburger');
+var links = header.querySelector('.nav-links');
+var close = header.querySelector('.close-btn');
 hamburger.addEventListener('click', function () {
 	links.classList.add('nav-open');
 	close.classList.add('close-hamburger');
