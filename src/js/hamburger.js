41a4b6930d0d3f8c285c6a4abbfe1791e9cb30c7
sumit-narang <sumitnarang.web@gmail.com>
2018-02-02 20:03:42 +0530

    Add no-srcoll when menu clicked on small screen

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -5,8 +5,10 @@ var close = header.querySelector('.close-btn');
 hamburger.addEventListener('click', function () {
 	links.classList.add('nav-open');
 	close.classList.add('close-hamburger');
+	document.body.style.overflow = "hidden";
 });
 close.addEventListener('click', function () {
 	links.classList.remove('nav-open');
 	close.classList.remove('close-hamburger');
+	document.body.style.overflow = "auto";
 });
