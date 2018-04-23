250e41ff4719de19d87e8451a980100c827a9c9b
sumit-narang <sumitnarang.web@gmail.com>
2018-04-23 18:21:33 +0530

    Integrated contact-us form with firebase database

--- resources/public/js/firebase.js
+++ resources/public/js/firebase.js
@@ -1,10 +1,53 @@
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyCiI4z4n7ql1C_PW3XhojtH2a--5kqh6aA",
-	authDomain: "fir-ep.firebaseapp.com",
-	databaseURL: "https://fir-ep.firebaseio.com",
-	projectId: "fir-ep",
-	storageBucket: "",
-	messagingSenderId: "588592635692"
+	apiKey: "AIzaSyBAQ7akghM1bJ3Rnhp0NxSFh95j5AOotXY",
+	authDomain: "tranquil-harbor-163720.firebaseapp.com",
+	databaseURL: "https://tranquil-harbor-163720.firebaseio.com",
+	projectId: "tranquil-harbor-163720",
+	storageBucket: "tranquil-harbor-163720.appspot.com",
+	messagingSenderId: "668417789037"
 };
 firebase.initializeApp(config);
+
+var contactsRef = firebase.database().ref("contacts");
+
+document.querySelector('#contact-form form').addEventListener('submit',function(e){
+    e.preventDefault();
+
+		//getting input
+		var datesubmit = new Date().toString();
+		console.log(datesubmit);
+    var name = getInputVal('form-name');
+    var email = getInputVal('form-email');
+    var phone = getInputVal('form-tel');
+    var msg = getInputVal('form-msg');
+
+    //save form to database
+    saveMsg(datesubmit, name, phone, email, msg);
+
+    document.getElementById('form-submitMsg').classList.add('show');
+
+    //displaying a modal for 3s
+    setTimeout(function(){
+        document.getElementById('form-submitMsg').classList.remove('show');
+    },3000);
+
+    //clear form
+    document.querySelector('#contact-form form').reset();
+});
+
+
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+
+function saveMsg(datesubmit, name, phone, email, msg){
+    var newContactsRef = contactsRef.push();
+    newContactsRef.set({
+				SubmitTime: datesubmit,
+				Name: name,
+        Email: email,
+        Phone: phone,
+        Message: msg
+    });
+}
